6b86b5c1be357bda3c8c695c5dece0e84d6fea9c
Denis Stepanov <22733222+denis-stepanov@users.noreply.github.com>
2021-11-11 00:39:20 +0100

    Lighten web interface construction code a bit

--- web.cpp
+++ web.cpp
@@ -8,6 +8,8 @@
 
 using namespace ds;
 
+#define web_page System::web_page          // To make the code a bit lighter
+
 // Data providers
 extern BulbManager bulb_manager;           // Bulb manager
 
@@ -17,39 +19,39 @@ extern BulbManager bulb_manager;           // Bulb manager
 void handleRoot() {
   const auto nabulbs = bulb_manager.getNumActive();
   System::pushHTMLHeader(System::hostname);
-  System::web_page += "<h3>Yeelight Button</h3>";
+  web_page += "<h3>Yeelight Button</h3>";
   if (nabulbs) {
-    System::web_page += "<p>Linked to the bulb";
+    web_page += "<p>Linked to the bulb";
     if (nabulbs > 1)
-      System::web_page += "s";
-    System::web_page += ":</p><p><ul>";
+      web_page += "s";
+    web_page += ":</p><p><ul>";
     bulb_manager.printStatusHTML();
-    System::web_page += "</ul></p>";
+    web_page += "</ul></p>";
   } else
-    System::web_page += "<p>Not linked to a bulb</p>";
-  System::web_page += "<p>[<a href=\"conf\">Change</a>]";
+    web_page += "<p>Not linked to a bulb</p>";
+  web_page += "<p>[<a href=\"conf\">Change</a>]";
   if (bulb_manager.getNumActive())
-    System::web_page += " [<a href=\"flip\">Flip</a>]";
-  System::web_page += " [<a href=\"log\">Log</a>]";
-  System::web_page += " [<a href=\"about\">About</a>]";
-  System::web_page += "</p><hr/><p><i>Connected to network ";
-  System::web_page += System::getNetworkName();
-  System::web_page += ", hostname ";
-  System::web_page += System::hostname;
-  System::web_page += ".local (";
-  System::web_page += System::getLocalIPAddress();
-  System::web_page += "), ";
-  System::web_page += System::getNetworkDetails();
-  System::web_page += ".</i></p>";
-  System::web_page += "<p><small><a href=\"";
-  System::web_page += System::app_url;
-  System::web_page += "\">";
-  System::web_page += System::app_name;
-  System::web_page += "</a> v";
-  System::web_page += System::app_version;
-  System::web_page += " build ";
-  System::web_page += System::app_build;
-  System::web_page += "</small></p>";
+    web_page += " [<a href=\"flip\">Flip</a>]";
+  web_page += " [<a href=\"log\">Log</a>]";
+  web_page += " [<a href=\"about\">About</a>]";
+  web_page += "</p><hr/><p><i>Connected to network ";
+  web_page += System::getNetworkName();
+  web_page += ", hostname ";
+  web_page += System::hostname;
+  web_page += ".local (";
+  web_page += System::getLocalIPAddress();
+  web_page += "), ";
+  web_page += System::getNetworkDetails();
+  web_page += ".</i></p>";
+  web_page += "<p><small><a href=\"";
+  web_page += System::app_url;
+  web_page += "\">";
+  web_page += System::app_name;
+  web_page += "</a> v";
+  web_page += System::app_version;
+  web_page += " build ";
+  web_page += System::app_build;
+  web_page += "</small></p>";
   System::pushHTMLFooter();
   System::sendWebPage();
 }
@@ -57,28 +59,28 @@ void handleRoot() {
 // Bulb discovery page
 void handleConf() {
   System::pushHTMLHeader((String)System::hostname + " conf");
-  System::web_page += "<h3>Yeelight Button Configuration</h3>";
-  System::web_page += "<p>[<a href=\"/conf\">Rescan</a>] [<a href=\"/save\">Unlink</a>] [<a href=\"..\">Back</a>]</p>";
-  System::web_page += "<p><i>Scanning ";
-  System::web_page += System::getNetworkName();
-  System::web_page += " for Yeelight devices...</i></p>";
-  System::web_page += "<p><i>Hint: turn all bulbs off, except the desired ones, in order to identify them easily.</i></p>";
+  web_page += "<h3>Yeelight Button Configuration</h3>";
+  web_page += "<p>[<a href=\"/conf\">Rescan</a>] [<a href=\"/save\">Unlink</a>] [<a href=\"..\">Back</a>]</p>";
+  web_page += "<p><i>Scanning ";
+  web_page += System::getNetworkName();
+  web_page += " for Yeelight devices...</i></p>";
+  web_page += "<p><i>Hint: turn all bulbs off, except the desired ones, in order to identify them easily.</i></p>";
 
   // Use chunked transfer to show scan in progress. Works in Chrome, not so well in Firefox
   System::web_server.setContentLength(CONTENT_LENGTH_UNKNOWN);
   System::sendWebPage();
   const auto num_bulbs = bulb_manager.discover();
-  System::web_page = "<p>Found ";
-  System::web_page += num_bulbs;
-  System::web_page += " bulb";
+  web_page = "<p>Found ";
+  web_page += num_bulbs;
+  web_page += " bulb";
   if (num_bulbs != 1)
-    System::web_page += "s";
-  System::web_page +=". Select bulbs to link from the list below.</p>";
-  System::web_page += "<form action=\"/save\"><p style=\"font-family: monospace;\">";
+    web_page += "s";
+  web_page +=". Select bulbs to link from the list below.</p>";
+  web_page += "<form action=\"/save\"><p style=\"font-family: monospace;\">";
   bulb_manager.printConfHTML();
-  System::web_page += "</p><p><input type=\"submit\" value=\"Link\"/></p></form>";
+  web_page += "</p><p><input type=\"submit\" value=\"Link\"/></p></form>";
   System::pushHTMLFooter();
-  System::web_server.sendContent(System::web_page);
+  System::web_server.sendContent(web_page);
   System::web_server.sendContent("");
   System::web_server.client().stop();   // Terminate chunked transfer
 }
@@ -91,22 +93,22 @@ void handleSave() {
   const auto nargs = System::web_server.args();
   const auto nabulbs = bulb_manager.getNumActive();
   System::pushHTMLHeader(System::hostname + nabulbs || !nargs ? " saved" : " error");
-  System::web_page += "<h3>Yeelight Button Configuration ";
-  System::web_page += nabulbs || !nargs ?  "Saved" : " Error";
-  System::web_page += "</h3>";
+  web_page += "<h3>Yeelight Button Configuration ";
+  web_page += nabulbs || !nargs ?  "Saved" : " Error";
+  web_page += "</h3>";
   if (nargs) {
     if (nabulbs) {
-      System::web_page += "<p>";
-      System::web_page += nabulbs;
-      System::web_page += " bulb";
+      web_page += "<p>";
+      web_page += nabulbs;
+      web_page += " bulb";
       if (nabulbs != 1)
-        System::web_page += "s";
-      System::web_page += " linked</p>";
+        web_page += "s";
+      web_page += " linked</p>";
     } else
-      System::web_page += "<p>Too many bulbs passed</p>";
+      web_page += "<p>Too many bulbs passed</p>";
   } else
-    System::web_page += "<p>Bulbs unlinked</p>";
-  System::web_page += "<p>[<a href=\"..\">Back</a>]</p>";
+    web_page += "<p>Bulbs unlinked</p>";
+  web_page += "<p>[<a href=\"..\">Back</a>]</p>";
   System::pushHTMLFooter();
   System::sendWebPage();
 }
@@ -119,9 +121,9 @@ void handleFlip() {
   System::appLogWriteLn(lmsg);
 
   System::pushHTMLHeader((const String)System::hostname + " flip");
-  System::web_page += "<h3>Yeelight Button Flip</h3>";
-  System::web_page += bulb_manager.getNumActive() ? "<p>Light flipped</p>" : "<p>No linked bulbs found</p>";
-  System::web_page += "<p>[<a href=\"/flip\">Flip</a>] [<a href=\"..\">Back</a>]</p>";
+  web_page += "<h3>Yeelight Button Flip</h3>";
+  web_page += bulb_manager.getNumActive() ? "<p>Light flipped</p>" : "<p>No linked bulbs found</p>";
+  web_page += "<p>[<a href=\"/flip\">Flip</a>] [<a href=\"..\">Back</a>]</p>";
   System::pushHTMLFooter();
   System::sendWebPage();
 }
