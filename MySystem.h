55242176484679351e4da19ec5b8729dd6923809
Denis Stepanov <22733222+denis-stepanov@users.noreply.github.com>
2021-11-26 01:01:50 +0100

    Update explanation of user settings

--- MySystem.h
+++ MySystem.h
@@ -4,12 +4,13 @@
 #ifndef _DS_SYSTEM_H_
 
 // === User configuration
-#define DS_TIMEZONE  TZ_Europe_Paris      // Timezone. Pick yours from https://github.com/esp8266/Arduino/blob/master/cores/esp8266/TZ.h
-#define DS_LATITUDE  51.483611            // Your latitude  (needed for solar-based timers)
-#define DS_LONGITUDE -0.005833            // Your longitude (needed for solar-based timers)
+//// Geographical coordinates do not have to match your location precisely. One decimal digit after a comma (NN.N) (few km precision) is good enough
+#define DS_TIMEZONE  TZ_Europe_Paris      // Your timezone (needed for clock timers). Pick from the list https://github.com/esp8266/Arduino/blob/master/cores/esp8266/TZ.h
+#define DS_LATITUDE  48.8584              // Your latitude (needed for timers triggered on sunrise / sunset)
+#define DS_LONGITUDE 2.2945               // Your longitude (needed for timers triggered on sunrise / sunset)
 #define DS_HOSTNAME  "ybutton1"           // <hostname>.local in the local network. Also, SSID of the temporary network for Wi-Fi configuration
 
-//// Different button wiring on various boards
+//// Different button wiring on various boards. Normally OK as it is
 ////// For Witty Cloud, use board "LOLIN(WEMOS) D1 R2 & mini"
 #ifdef ARDUINO_ESP8266_WEMOS_D1MINI
 #define BUTTON_BUILTIN D2
@@ -17,7 +18,7 @@
 ////// For NodeMCU, use board "NodeMCU 1.0 (ESP-12E Module)". Defaults are fine
 ////// For ESP-01(S), use board "Generic ESP8266 Module"; Flash Size "1MB (FS:256KB)"; Builtin LED: 1 for ESP-01, 2 for ESP-01S. Connect a push button between GPIO0 and GND
 
-//// #define BUTTON_BUILTIN 0             // Uncomment and define if your button is connected to a GPIO other than 0
+// #define BUTTON_BUILTIN 0               // Uncomment and define if your button is connected to a GPIO other than 0
 // === End of user configuration
 
 // Changing below this line will require code adaptations elsewhere in the app
